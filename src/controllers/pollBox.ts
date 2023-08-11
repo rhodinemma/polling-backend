c275fcf0bfeb23ecb94093047b0ef4f575ab7956
rhodinemma <rhodinemma10@gmail.com>
2023-08-11 15:51:53 +0300

    chore: apply formatting

--- src/controllers/pollBox.ts
+++ src/controllers/pollBox.ts
@@ -3,26 +3,26 @@ import { isEmpty } from "lodash";
 import AppRes from "../types/AppRes";
 import * as PollBox from "../models/pollBox";
 
-export const updatePollBox = async (req:Request, res: Response , next: NextFunction) => {
-    try{
-        const { option }  = req.body;
+export const updatePollBox = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { option } = req.body;
         const pollId = req.params.id;
-        if(isEmpty(option) || !pollId){
+        if (isEmpty(option) || !pollId) {
             const errRes: AppRes = {
-                data : {},
-                isError : true,
+                data: {},
+                isError: true,
                 errMsg: 'Please add the option and pollId, they are required',
             };
             res.status(400).send(errRes);
-            return ;
+            return;
         }
-        const pollBox = await PollBox.update(pollId,option,1);
-        const response : AppRes = {
-            data: {pollBox},
+        const pollBox = await PollBox.update(pollId, option, 1);
+        const response: AppRes = {
+            data: { pollBox },
             isError: false,
         };
         res.send(response);
-    }catch(error){
+    } catch (error) {
         next(error);
-    }     
+    }
 };
